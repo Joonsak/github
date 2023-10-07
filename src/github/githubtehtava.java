c3786a3ad8c39330e253e38d500e38d48196c72e
Joonsak <joona22010@student.hamk.fi>
2023-10-07 13:24:09 +0300

    looping
    Lisäsin do while loopin, että pelaaja voi arvata uudelleen ja annoin mahdollisuuden kirjoittaa "loppu", että voi lopettaa pelin.

--- src/github/githubtehtava.java
+++ src/github/githubtehtava.java
@@ -6,14 +6,22 @@ public class githubtehtava {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
 		String name = "Emma";
-		
-		
+		String guess;
 		System.out.println("Guess my name!");
-		String guess = in.nextLine();
+
+	do
+	{
+		guess = in.nextLine();
 		if(name.equals(guess)) {
 			System.out.println("Congratulations you guessed right.");
+			break;
 		}
-		
+		if(guess.equals("loppu")) {
+			break;
+		}
+		System.out.println("Guess again. (kirjoita: loppu, niin ohjelma sammuu.");
+	}while(!name.equals(guess));
+	
 	}
 
 }
